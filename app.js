49b6f698f9a5c61049d73bb9628a431335631f58
rohanvachhani <rohan.vachhani5@gmail.com>
2023-05-06 21:25:20 -0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,12 @@
+//Misc Helpers
+function randomFromArray(array) {
+    return array[Math.floor(Math.random() * array.length)];
+}
+
+function getKeyString(x, y) {
+    return `${x}x${y}`;
+}
+
+(function () {
+
+})();
